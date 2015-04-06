d334c86f89898cf72f9678cb83ebf1280da9ce7c
Siwadon Saosoong <siwadon.s@gmail.com>
2015-04-06 19:33:32 +0700

    Fix code style from jshint

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-var graph = require('fbgraph')
-var RSS   = require('rss')
-var Hapi  = require('hapi')
+var graph = require('fbgraph');
+var RSS   = require('rss');
+var Hapi  = require('hapi');
 
-var server = new Hapi.Server()
-server.connection({  
+var server = new Hapi.Server();
+server.connection({
     port: process.env.PORT || 8000
-})
+});
 
-graph.setAccessToken(process.env['ACCESS_TOKEN'])
+graph.setAccessToken(process.env.ACCESS_TOKEN);
 
 var options = {
-    timeout:  5000
-  , pool:     { maxSockets: Infinity }
-  , headers:  { connection: "keep-alive" }
-}
+    timeout:  5000,
+    pool:     { maxSockets: Infinity },
+    headers:  { connection: "keep-alive" }
+};
 
 server.route({
     method: 'GET',
     path:'/{id}', 
     handler: function (request, reply) {
-        var id = parseInt(request.params.id)
+        var id = parseInt(request.params.id);
         if (id) {
             graph
                 .setOptions(options)
                 .get(id + "/feed", function(err, res) {
                     if (err) {
-                        reply(err)
-                        return
+                        reply(err);
+                        return;
                     }
                     if (res.data.length === 0) {
-                        reply("no data")
-                        return
+                        reply("no data");
+                        return;
                     }
                     try {
                         var data = res.data[0];
@@ -41,10 +41,10 @@ server.route({
                             language: 'th',
                             feed_url: 'http://facefeed.herokuapp.com/' + id,
                             ttl: 10
-                        }
-                        var feed = new RSS(feedOptions)
+                        };
+                        var feed = new RSS(feedOptions);
                         res.data.forEach(function(item) {
-                            body = item.message || item.story || ''
+                            body = item.message || item.story || '';
                             feed.item({
                                 title: body.substring(0, 140),
                                 description: body,
@@ -55,16 +55,16 @@ server.route({
                         reply(feed.xml());
                     }
                     catch(ex) {
-                        reply("problem generating feed: " + ex)
+                        reply("problem generating feed: " + ex);
                     }
-                })
+                });
         }
         else {
-            reply(request.params.id + ' is not a group id. you can find by going to <a href="http://lookup-id.com/">lookup-id.com</a>')
+            reply(request.params.id + ' is not a group id. you can find by going to <a href="http://lookup-id.com/">lookup-id.com</a>');
         }
     }
-})
+});
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
-})
+});
--- ping.js
+++ ping.js
@@ -1,16 +1,16 @@
-var http = require("http")
+var http = require("http");
 
 var options = {
   host: 'facefeed.herokuapp.com',
   port: 80,
-}
+};
 
 var req = http.request(options, function(res) {
   console.log('facefeed status: ' + res.statusCode);
-})
+});
 
 req.on('error', function(e) {
   console.log('problem with request: ' + e.message);
-})
+});
 
-req.end()
+req.end();
